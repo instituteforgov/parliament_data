07ef2ce13d0590330d4536529931f99a75e2ddee
philipnye <philipnye@users.noreply.github.com>
2024-03-24 18:55:41 +0000

    Save results to d/b

--- parliament_data/identify_changes.py
+++ parliament_data/identify_changes.py
@@ -11,23 +11,52 @@
             - URL to JSON file containing previous data
         - SQL: 'core.person'
     Outputs
-        TODO
+        - SQL: workflow.task
+            - See research database documentation
+        - SQL: workflow.task_status
+            - See research database documentation
+        - SQL: workflow.task_allocation
+            - See research database documentation
+        - SQL: workflow.table
+            - See research database documentation
+        - SQL: workflow.{uuid}
+            - See research database documentation
+            - NB: In practice this is a UUID-based string, rather than a UUID
+            as the format of a UUID is not a valid format for a SQL identifier
     Parameters
-        XXX
+        - members_url: URL of JSON file containing new data
     Notes
         None
 '''
 
-# import os
+import os
+import uuid
 
 import pandas as pd
+from sqlalchemy import text
 
-# from ds_utils import database_operations as dbo
+from ds_utils import database_operations as dbo
 from ds_utils import dataframe_operations as dfo
+
+# %%
+# SET HOLD
+raise SystemExit(0)
 
 # %%
 # SET PARAMETERS
 members_url = 'https://raw.githubusercontent.com/instituteforgov/parliament_data/d8f69ff32c66f0f7e606d399e2f34261ce14d8f5/data/current_mps.json'      # noqa: E501
+
+# %%
+# CONNECT TO DATABASE
+connection = dbo.connect_sql_db(
+    driver='pyodbc',
+    driver_version=os.environ['odbc_driver'],
+    dialect='mssql',
+    server=os.environ['odbc_server'],
+    database=os.environ['odbc_database'],
+    authentication=os.environ['odbc_authentication'],
+    username=os.environ['odbc_username'],
+)
 
 # %%
 # READ IN DATA
@@ -37,15 +66,6 @@ df_members = pd.read_json(members_url)
 # D/b data
 # FIXME: This currently uses a previous version of df_members in place of df_person_existing
 # df_members_ hasn't been reshaped to match the structure we intend to use in the database yet
-# connection = dbo.connect_sql_db(
-#     driver='pyodbc',
-#     driver_version=os.environ['odbc_driver'],
-#     dialect='mssql',
-#     server=os.environ['odbc_server'],
-#     database=os.environ['odbc_database'],
-#     authentication=os.environ['odbc_authentication'],
-#     username=os.environ['odbc_username'],
-# )
 
 # df_person_existing = dbo.retry_sql_function(
 #     function=pd.read_sql,
@@ -64,8 +84,7 @@ df_person_existing = pd.read_json(person_existing_url)
 
 # %%
 # IDENTIFY CHANGES
-# Identify changes between JSON file and SQL table
-dfo.identify_row_differences(
+df_diff = dfo.identify_row_differences(
     df_members,
     df_person_existing,
     how='outer',
@@ -74,5 +93,82 @@ dfo.identify_row_differences(
     keep_rows='both',
     validate='1:1',
 )
+
+# %%
+# SAVE CHANGES TO D/B
+# Create new task
+task_id = uuid.uuid4()
+
+query = f'''
+    insert into workflow.task values (
+        \'{task_id}\',
+        'sql_upsert',
+        'Review identify_changes.py output'
+    )
+'''
+
+with dbo.retry_sql_function(connection.begin) as conn:
+    conn.execute(text(query))
+
+# %%
+# Create new task status
+user_creator = os.environ['odbc_username']
+
+query = f'''
+    insert into workflow.task_status values (
+        newid(),
+        \'{task_id}\',
+        'created',
+        \'{user_creator}\',
+        getdate()
+    )
+'''
+
+with dbo.retry_sql_function(connection.begin) as conn:
+    conn.execute(text(query))
+
+# %%
+# Create new task allocation
+user_reviewer = os.environ['odbc_username']
+
+query = f'''
+    insert into workflow.task_allocation values (
+        newid(),
+        \'{task_id}\',
+        'reviewer',
+        \'{user_reviewer}\'
+    )
+'''
+
+with dbo.retry_sql_function(connection.begin) as conn:
+    conn.execute(text(query))
+
+# %%
+# Create new table
+
+# Create table
+# Converting dashes to underscores to make this a valid identifier and
+# appending a prefix because identifiers can't start with a number
+name = 't_' + str(uuid.uuid4()).replace('-', '_')
+
+df_diff.to_sql(
+    name,
+    connection,
+    schema='workflow',
+    index=False,
+)
+
+# Save details of table created
+query = f'''
+    insert into workflow.[table] values (
+        newid(),
+        \'{task_id}\',
+        'for_review',
+        \'{name}\'
+    )
+'''
+
+with dbo.retry_sql_function(connection.begin) as conn:
+    conn.execute(text(query))
 
 # %%
